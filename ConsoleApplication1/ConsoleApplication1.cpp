aff4d3db913fa13b36eb8da178c10e341a91d751
VKONSTANTINIUS <kosrounder@gmail.com>
2025-03-01 01:03:48 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    SetConsoleOutputCP(1251);
+
+    cout << "\tРЕЗЮМЕ\n\nВАСИЛЕНКО КОСТЯНТИН ОЛЕГОВИЧ\n\n";
+    cout << "Вік\t\t33 роки\nМісто\t\tЧеркаси,Україна\nТелефон\t\t+380632666269\nЕл.пошта\tkosrounder@gmail.com\n\n";
+
+    cout << "\t\tОСВІТА\n\n";
+    cout << "- Черкаський інститут банківської справи \nУніверситету банківської справи \nНаціонального банку України\n";
+    cout << "  Облік і аудит - Магістр\n\n";
+    cout << "- Академія IT Step\n  Розробка програмного забезпечення\n\n";    
+
+    cout << "\t\tДОСВІД РОБОТИ\n\n";
+    cout << "ТОВ ЧЕРКАСИХЛІБ ТД \nПровідний економіст з фінансової роботи\n з 2012 року\n\n";
+
+    cout << "\t\tЗНАННЯ ТА НАВИЧКИ\n\n";
+    cout << "Python\nHTML\nCSS\n\tGREED LAYOUT\n\tFLEXBOX\n";
+
+
+}
+
